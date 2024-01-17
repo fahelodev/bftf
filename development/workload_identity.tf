347b29541604e88c5fccca04d81238d8383c0625
fahelodev <fahelo.dev@gmail.com>
2024-01-17 13:07:08 -0300

    development - v.1.0.0

---
+++ development/workload_identity.tf
@@ -0,0 +1,16 @@
+resource "azurerm_user_assigned_identity" "dev_test" {
+  name                = "dev-test"
+  location            = azurerm_resource_group.barriofarma_rg.location
+  resource_group_name = azurerm_resource_group.barriofarma_rg.name
+}
+
+resource "azurerm_federated_identity_credential" "dev_test" {
+  name                = "dev-test"
+  resource_group_name = azurerm_resource_group.barriofarma_rg.name
+  audience            = ["api://AzureADTokenExchange"]
+  issuer              = azurerm_kubernetes_cluster.aks.oidc_issuer_url
+  parent_id           = azurerm_user_assigned_identity.dev_test.id
+  subject             = "system:serviceaccount:dev:my-account"
+
+  depends_on = [azurerm_kubernetes_cluster.aks]
+}
